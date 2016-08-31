e86c91624508b567feeba0650ac8f66acea22449
常鸣 <px.zhangyang@gmail.com>
2016-09-01 00:47:31 +0800

    up

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # 12306-cli
 
-看到网上有个小伙伴用 python 实现了一款火车票查询工具，因此萌生了用 JavaScript 实现的冲动。
+![](./screenshot.png)
+
+看到网上有个小伙伴用 python 实现了一款火车票查询工具，因此萌生了用 JavaScript 实现的冲动。此工具在 osx 下开发，windows 尚未测试。
+
+## !!待完成
+
+* 出发地和到达站是写死的，还没开始搞。
+
+## 用到的工具
+
+* chalk - 命令行字体加色
+* request - http client
+* commander - 命令行程序框架
+* columnify - 命令行表格格式化工具
 
 ## 安装
 
@@ -13,5 +26,5 @@
 ```bash
 > 12306 query hangzhou xuzhou 2016-09-20
 
-> 12306 query 杭州 徐州 2016-09-20,2016-09-31
+> 12306 query 杭州 徐州 2016-09-20
 ```
--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,13 @@
+#!/usr/bin/env node
+
+/**
+ * 12306-cli index.js
+ *
+ * Author: zhangyang <mailto:px.zhangyang@gmail.com>
+ */
 const commander = require('commander');
-const pkgInfo = require('./package.json');
-const queryFunc = require('./lib/query');
+const pkgInfo = require('../package.json');
+const queryFunc = require('../lib/query');
 const commandParser = function(afterParseFunc) {
   return function(frm, to, at) {
     afterParseFunc.call(this, frm, to, at);
@@ -9,18 +16,18 @@ const commandParser = function(afterParseFunc) {
 const program = commander.version(pkgInfo.version);
 
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // http://stackoverflow.com/questions/20433287/node-js-request-cert-has-expired
+
+
 program
   .command('query <from> <to> <time>')
   .alias('q')
   .description('查询指令')
   .action(commandParser(queryFunc))
-  // .option('-f, --from', '出发地')
+  .option('--gaotie', '只看高铁')
   // .option('-d, --destination', '目的地')
   // .option('-a, --at', '具体日期');
 
 
 program
   .parse(process.argv);
-
-
-console.log(`\n\n12306 command line tools v.${pkgInfo.version}`);
